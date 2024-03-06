a7c019b27bc5f9c471b9b5021fbe838556d0e378
JyothirmayiMunji-tester <108141239+JyothirmayiMunji-tester@users.noreply.github.com>
2024-03-06 12:38:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -31,17 +31,17 @@ variable "location_name" {
 
 variable "resource_group_name" {
   description = "Azure Resource Group name"
-  default = "smithaciactsrg"
+  default = "jyothirmayiciactsrg"
 }
 
 variable "mysql_server_name" {
   description = "Azure MySQL Server name"
-  default = "smithaciactsmysql"
+  default = "jyothirmayiciactsmysql"
 }
 
 variable "mysql_server_login" {
   description = "Azure MySQL Server Login User"
-  default = "smithaciactsuser"
+  default = "jyothirmayiciactsuser"
 }
 
 variable "mysql_server_password" {
